d63edaefdbbf4b5c8a58087c215518de3c872c92
sobhi007 <72100489+sobhi007@users.noreply.github.com>
2021-07-04 20:03:18 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,29 +1,28 @@
+
 <?php
-// $to = "mohamed.sobhi1304@gmail.com";
-// $subject = "test";
-
-// $message = 'Dear '."sobhi".',<br>';
-// $message .= "We welcome you to be part of family<br><br>";
-// $message .= "Regards,<br>";
-
-// // Always set content-type when sending HTML email
-// $headers = "MIME-Version: 1.0" . "\r\n";
-// $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// // More headers
-// $headers .= 'From: <mohamed.sobhi1303@gmail.com>' . "\r\n";
-
-
-// mail($to,$subject,$message,$headers);
-
-
-$to = 'mohamed.sobhi1304@gmail.com';
-$subject = 'Hello from XAMPP!';
-$message = 'This is a test';
-$headers = "From: mohamed.sobhi1303@gmail.com\r\n";
-if (mail($to, $subject, $message, $headers)) {
-   echo "SUCCESS";
-} else {
-   echo "ERROR";
+  
+$x=7;
+$y=3;
+$operator = '8'; 
+switch ($operator) {
+   case '+':
+   echo $x + $y;
+      break;
+   case '-':
+      echo $x - $y; 
+      break;   
+   case '*':
+      echo $x * $y; 
+      break; 
+   case '/':
+      echo $x / $y; 
+      break;           
+   default:
+      echo '<span style="color:red">invalied';
+      break;
 }
+
+
+
+
 ?>
